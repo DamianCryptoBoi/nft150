5965b1e0425dbd14ab6225d9c92686b517a177fc
Nguyen Binh Nguyen <nguyen.nguyen@sotatek.com>
2021-08-13 14:38:54 +0700

    update deploy eth

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,7 @@ module.exports = {
       }
     }
   },
-  defaultNetwork: 'hardhat',
+  defaultNetwork: 'rinkeby',
   networks: {
     hardhat: {
         gas: 12000000,
@@ -38,15 +38,19 @@ module.exports = {
       url: 'https://ropsten.infura.io/v3/fca5c00ba9a04c7c934d026632ed868e',
       accounts: [`0xc5f4ffe418d0ce5a33fdb6444d66484feed262d099f83dd404f9b8f8ac36c016`]
     },
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/fca5c00ba9a04c7c934d026632ed868e',
+      accounts: [`9b1a1461a714724dbf4e8d2345cf5008545e1140f54914a0c1a62eb1bf1c88a0`]
+    },
 
     testbnb: {
       url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
       accounts: ['2a7b162564c6ca43e6289d48757bc12261339baa0b7a96271f0e0f99ed52e7a0'],
-      apiKey: "9E36GDSJK15GYXXN85186T5GDQI32B29MF"
+
     }
   },
 
   etherscan: {
-    apiKey: "9E36GDSJK15GYXXN85186T5GDQI32B29MF"
+    apiKey: "GUNCG9NJ78N2UW5WZSEUC82CQARFRVSWF5"
   }
 };
