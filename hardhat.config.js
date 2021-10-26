2f1366a1d727b26dcda765087040f89ce43f7aaa
NguyenNB <nguyen.nguyen@sotatek.com>
2021-10-26 09:51:55 +0700

    config move to local

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,6 @@ require("hardhat-abi-exporter");
 require("dotenv").config();
 
 module.exports = {
-
-
   solidity: {
     version: "0.8.6",
     settings: {
@@ -27,44 +25,17 @@ module.exports = {
         allowUnlimitedContractSize: true,
         timeout: 1800000
     },
-    testnet: {
-      //url: "https://data-seed-prebsc-2-s1.binance.org:8545",
-      //accounts: [process.env.PRIVATE_KEY],
-      url: 'https://ropsten.infura.io/v3/fca5c00ba9a04c7c934d026632ed868e',
-      accounts: [`0xc5f4ffe418d0ce5a33fdb6444d66484feed262d099f83dd404f9b8f8ac36c016`],
-    },
-    ropsten: {
-      //url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      url: 'https://ropsten.infura.io/v3/fca5c00ba9a04c7c934d026632ed868e',
-      accounts: [`0xc5f4ffe418d0ce5a33fdb6444d66484feed262d099f83dd404f9b8f8ac36c016`]
-    },
     rinkeby: {
-      url: 'https://rinkeby.infura.io/v3/fca5c00ba9a04c7c934d026632ed868e',
-      accounts: [`9b1a1461a714724dbf4e8d2345cf5008545e1140f54914a0c1a62eb1bf1c88a0`],
+      url: process.env.PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY],
       gas: 12000000,
       blockGasLimit: 0x1fffffffffffff,
       allowUnlimitedContractSize: true,
       timeout: 1800000
-    },
-    mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
-      accounts: [`9b1a1461a714724dbf4e8d2345cf5008545e1140f54914a0c1a62eb1bf1c88a0`],
-      gas: 12000000,
-      blockGasLimit: 0x1fffffffffffff,
-      allowUnlimitedContractSize: true,
-      timeout: 1800000
-    },
-
-    testbnb: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-      accounts: ['2a7b162564c6ca43e6289d48757bc12261339baa0b7a96271f0e0f99ed52e7a0'],
-
     }
   },
 
   etherscan: {
-    apiKey: "GUNCG9NJ78N2UW5WZSEUC82CQARFRVSWF5" //eth
-    //apiKey: "7A5TAKHWI24BHVT5V8CSUDRPVAEM5KUWWP" // poligon mumbai
-
+    apiKey: process.env.ETHERSCAN_APIKEY
   }
 };
--- scripts/deploy-ether.js
+++ scripts/deploy-ether.js
@@ -16,12 +16,6 @@ const main = async () => {
       await polka721General.deployed();
       console.log("POLKA721_CONTRACT deployed at: ", polka721General.address);
 
-      //polka1155
-//      const Polka1155 = await hre.ethers.getContractFactory("Polka1155");
-//      const polka1155 = await Polka1155.deploy();
-//      await polka1155.deployed();
-//      console.log("POLKA1155_CONTRACT (is contract of farm)  deployed at: ", polka1155.address);
-
 //    //NFT150
       const NFT150 = await hre.ethers.getContractFactory("NFT150");
       const nft150 = await NFT150.deploy();
@@ -40,48 +34,9 @@ const main = async () => {
       await polkaReferral.deployed();
       console.log("REFERRAL_CONTRACT deployed at: ", polkaReferral.address);
 
-      //POLKAExchangeV2
-//      const PolkaExchangeV2 = await hre.ethers.getContractFactory("PolkaExchangeV2");
-//      const polkaExchangeV2 = await POLKAExchangeV2.deploy(
-//        "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-//        "0xEA040dB91b2FB439857145D3e660ceE46f458F94",
-//        "0x3b00ef435fa4fcff5c209a37d1f3dcff37c705ad",
-//        "0xc778417e063141139fce010982780140aa0cd5ab"
-//      );
-//      await polkaExchangeV2.deployed();
-//      console.log("POLKAV2xchange deployed at: ", polkaExchangeV2.address);
-//      await polkaExchangeV2.setUsdtMarket("0xEA040dB91b2FB439857145D3e660ceE46f458F94");
-
-      //ProfitEstimator
-//      const ProfitEstimator = await hre.ethers.getContractFactory("ProfitEstimator");
-//      const profitEstimator = await ProfitEstimator.deploy(marketV3.address, polkaExchangeV2.address);
-//      await profitEstimator.deployed();
-//      console.log("ProfitEstimator deployed at: ", profitEstimator.address);
-
-//      //BNFT150Token
-//      const BNFT150Token = await hre.ethers.getContractFactory("BNFT150Token");
-//      const bNFT150Token = await BNFT150Token.deploy();
-//      await bNFT150Token.deployed();
-//      console.log("POLKA_CONTRACT deployed at: ", bNFT150Token.address);
-
-
-      //0xa0e7fe8176135C12657f54F41DF5851F22C868B3 polka721General.address
-
       await marketV3.setReferralContract(polkaReferral.address);
-//      await marketV3.addPOLKANFTs(polka1155.address, true, true);
       await marketV3.addPOLKANFTs(polka721General.address, true, false);
       await marketV3.addPOLKANFTs(nft150.address, true, false);
-
-      //StakingPool
-//      const StakingPool = await hre.ethers.getContractFactory("StakingPool");
-//      const stakingPool = await StakingPool.deploy(nft150.address, bNFT150Token.address);
-//      await stakingPool.deployed();
-//      console.log("STAKING_CONTRACT deployed at: ", stakingPool.address);
-
-//    Testnet
-
-//	address public USDT = 0x584119951fA66bf223312A29FB6EDEBdd957C5d8;
-//	address public WBNB = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
 
       await marketV3.setPaymentMethod(
             "0xEA040dB91b2FB439857145D3e660ceE46f458F94", // usdt
