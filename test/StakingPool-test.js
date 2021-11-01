7e6ef2f79566fdbd1a6836969977942dc2b951d1
nguyen <nguyen.nguyen@sotatek.com>
2021-11-01 15:19:58 +0700

    delete unused

--- test/StakingPool-test.js
+++
@@ -1,8 +0,0 @@
-const { expect, assert, use } = require("chai");
-const { solidity } = require("ethereum-waffle");
-use(solidity);  //to user revertedWith
-
-describe("Unit testing - PolkaReferral", function() {
-   //TODO waiting confirm spec customer
-
-});
