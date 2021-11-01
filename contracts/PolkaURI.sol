941c1e38a632fbe10a01a4a6a7e744e86c68c421
nguyen <nguyen.nguyen@sotatek.com>
2021-11-01 09:46:30 +0700

    add file

---
+++ contracts/PolkaURI.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./token/ERC1155Tradeble.sol";
+
+/**
+ * @title 1155 General
+ * 1155General
+ */
+contract NFT150 is ERC1155Tradeble {
+    constructor() ERC1155Tradeble("NFT150 General", "NFT150")
+    {
+        _setBaseMetadataURI("https://yng30mk417.execute-api.ap-southeast-1.amazonaws.com/v1/"); //TODO
+    }
+}
---
+++ contracts/interfaces/IPolkaURI.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+interface IAddressMarket {
+
+
+    function marketAddress() external returns(address);
+
+
+}
