6778b047c486f9c7b51c19707195108b153af7e7
nguyen <nguyen.nguyen@sotatek.com>
2021-12-17 15:44:18 +0700

    deploy

---
+++ scripts/update-auctionv3.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat")
+
+const main = async () => {
+      const [admin] = await hre.ethers.getSigners();
+
+      const AuctionV3 = await hre.ethers.getContractFactory("AuctionV3");
+      const auctionV3 = await AuctionV3.deploy();
+      await auctionV3.deployed();
+      console.log("AUCTION_CONTRACT deployed at: ", auctionV3.address);
+
+      await auctionV3.setPaymentMethod("0xd35d2e839d888d1cDBAdef7dE118b87DfefeD20e", true); // usdt
+      await auctionV3.setPaymentMethod("0x0000000000000000000000000000000000000000", true); //eth
+      await auctionV3.setPaymentMethod("0xbec758b709075141c71e1011b3E5ecea9c3cbc0b", true); //xp
+      console.log("Finish!!!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
